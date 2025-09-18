1c2c32b7b76ff0ea8f0ccb5f5b0c68cade7e9037
AkuaAmofa <144915602+AkuaAmofa@users.noreply.github.com>
2025-09-18 08:51:30 +0000

    work

---
+++ EvenCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenCheck;
+    public static void main(String[] args) {
+        Scanner input = newScanner(System.in);
+
+        System.out.println("Enter an integer")
+        int number = input.nextInt();
+
+        if (number % 5 == 0){
+            System.out.println("HiHigh");
+        }
+
+        if (number % 2 ==0){
+            System.out.println("HiEven")
+        
+        }
+        input.close();mn
+    }
